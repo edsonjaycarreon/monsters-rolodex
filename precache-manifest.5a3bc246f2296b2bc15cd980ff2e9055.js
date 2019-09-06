e4123536879cad3a943791901fdddfec3f09ac8a
edsonjaycarreon <ejay.carreon@gmail.com>
2019-09-06 18:17:43 +0800

    Updates

---
+++ precache-manifest.5a3bc246f2296b2bc15cd980ff2e9055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1feb9b4be51e740ea8f42a5d326b0d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d18bc964ac07f17f8559",
+    "url": "/monsters-rolodex/static/css/main.16d5fa5d.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "d18bc964ac07f17f8559",
+    "url": "/monsters-rolodex/static/js/main.3372735a.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
